d080e6fbb3938d59ac66b910d27a3d8fe7698b76
DP4life <43859685+DP4life@users.noreply.github.com>
2018-11-06 17:10:04 +0200

    homework

---
+++ sas.js
@@ -0,0 +1,23 @@
+var x = 4;
+console.log(typeof x);
+var s = "damn";
+console.log(typeof s);
+var a = true;
+console.log(typeof a);
+var l = null;
+console.log(typeof l);
+var e;
+console.log(typeof e);
+var o = { type: "s", color: "white" };
+console.log(typeof o);
+
+var myCurrentAge = 15;
+if (myCurrentAge < 25) {
+    console.log("i am under 25");
+}
+else if (myCurrentAge = 25) {
+    console.log("i am 25");
+}
+else if (myCurrentAge > 25) {
+    console.log("i am over 25");
+}
